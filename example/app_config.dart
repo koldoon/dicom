77c6b117dd2d04f60bb29abddf3048666fb6c413
koldoon <ikoldoon@gmail.com>
2023-04-14 03:31:05 +0300

    Better example format

--- example/app_config.dart
+++
@@ -1,8 +0,0 @@
-abstract class AppConfig {
-  String get baseUrl;
-}
-
-class DevelopmentAppConfig implements AppConfig {
-  @override
-  String get baseUrl => 'https://dev-backend-domain.dev';
-}
--- example/main.dart
+++ example/main.dart
@@ -1,14 +1,69 @@
 import 'dart:async';
+import 'dart:io';
 import 'dart:isolate';
 
+import 'package:dicom/dicom.dart';
 import 'package:logecom/logecom.dart';
-
-import 'app_config.dart';
-import 'app_context.dart';
-import 'app_controller.dart';
 
 /// Simple console application design for simple DICom Dependency injection
 /// library usage demonstration
+
+/// Application config dependency interface example
+abstract class AppConfig {
+  String get baseUrl;
+}
+
+class DevelopmentAppConfig implements AppConfig {
+  @override
+  String get baseUrl => 'https://dev-backend-domain.dev';
+}
+
+/// Main Application Context
+/// Services instances and their dependencies
+/// It is very handy to extend [DI] class itself in this case.
+class AppContext extends DI {
+  AppContext._(this.config) {
+    // This allows generate individual loggers with category name based on target Class
+    bind(to: (c) => Logecom.createLogger(c.plan[c.plan.length - 2]), dynamic: true);
+    bind(to: (c) => AppController(appConfig: get(), logger: get()));
+    bind<AppConfig>(to: (c) => config);
+  }
+
+  /// [AppConfig] instance is created outside the context because it may contain
+  /// extra configuration params that might be used and applied before [AppContext.init].
+  /// For example, logging must be configured before the context since
+  /// context itself may have logic and errors to report during the initialization.
+  static AppContext init(AppConfig appConfig) {
+    instance = AppContext._(appConfig);
+    return instance;
+  }
+
+  /// Singleton [AppContext] instance - all the application context "holder"
+  /// Must be created with [AppContext.init] before usage
+  static late final AppContext instance;
+
+  final AppConfig config;
+}
+
+/// Syntactic sugar to use everywhere at desire.
+T inject<T>() {
+  return AppContext.instance.get<T>();
+}
+
+/// Example of typical dependencies consumer
+class AppController {
+  AppController({required this.appConfig, required this.logger});
+
+  /// AppConfig dependency
+  AppConfig appConfig;
+  Logger logger;
+
+  void runApp() {
+    logger.info('Starting the application');
+    // Your application code here
+    exit(0);
+  }
+}
 
 void main() {
   final config = DevelopmentAppConfig();
